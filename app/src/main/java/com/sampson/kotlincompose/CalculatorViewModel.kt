af1ac983c794a95808dba2e54abb2242eb93b472
fsampson30 <fsampson30@gmail.com>
2022-05-25 18:55:13 -0300

    Created calculator operation classes.

---
+++ app/src/main/java/com/sampson/kotlincompose/CalculatorViewModel.kt
@@ -0,0 +1,12 @@
+package com.sampson.kotlincompose
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel : ViewModel() {
+
+    var state by mutableStateOf(CalculatorState())
+        private set
+}
--- app/src/main/java/com/sampson/kotlincompose/MainActivity.kt
+++ app/src/main/java/com/sampson/kotlincompose/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.sampson.kotlincompose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.sampson.kotlincompose.ui.theme.KotlinComposeTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            KotlinComposeTheme {
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+
+            }
+        }
     }
 }
+
