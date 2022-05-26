5d5d5846f586bd7d4cee8abcc8eb1bc386edf6a1
fsampson30 <fsampson30@gmail.com>
2022-05-26 07:51:26 -0300

    UI created.

--- app/src/main/java/com/sampson/kotlincompose/MainActivity.kt
+++ app/src/main/java/com/sampson/kotlincompose/MainActivity.kt
@@ -3,16 +3,14 @@ package com.sampson.kotlincompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sampson.kotlincompose.ui.theme.KotlinComposeTheme
+import com.sampson.kotlincompose.ui.theme.MediumGray
 
 class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +20,15 @@ class MainActivity2 : ComponentActivity() {
                 val viewModel = viewModel<CalculatorViewModel>()
                 val state = viewModel.state
                 val buttonSpacing = 8.dp
-
+                com.sampson.kotlincompose.Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
             }
         }
     }
